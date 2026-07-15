c9fec074df32ec1a3d90c532fa43070eaebc07d5
Haikal <mnurmahaikal106@gmail.com>
2026-07-15 17:01:34 +0700

    Fix missing GetStorage import in NotificationController

--- lib/app/modules/notification/controllers/notification_controller.dart
+++ lib/app/modules/notification/controllers/notification_controller.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
+import 'package:get_storage/get_storage.dart';
 import 'package:smart_sme_app/app/data/api_config.dart';
 
 class NotificationController extends GetxController {
